c1c21b6298804c53a5d174f9af2d222676206860
caioccd <caioccd@icloud.com>
2015-08-30 20:47:09 -0300

    Covering everything from first chapter

--- delivery01/main.r
+++ delivery01/main.r
@@ -1,3 +1,20 @@
 source("load.R")
 
 load(CENSUS94_PATH)
+
+# Covering everything from the first chapter
+
+head(census94)
+summary(census94)
+
+numeric_cols <- c("age", "fnlwgt", "education_num", "capital_gain", "capital_loss", "hours_per_week")
+
+cov(census94[numeric_cols])
+var(census94[numeric_cols])
+cor(census94[numeric_cols])
+
+dist(scale(census94[1:14, numeric_cols], center = FALSE))
+
+sapply(numeric_cols, function(s) { qqnorm(census94[, s], main = s); qqline(census94[, s]) } )
+
+# Covering everything from the second chapter
--- preprocess.R
+++ preprocess.R
@@ -1,7 +1,7 @@
 source("load.R")
 
 # Join raw data into a single file
-census94 <- data.frame(do.call(rbind, lapply(list.files(RAW_DATA_PATH), function(s) read.csv(paste0(RAW_DATA_PATH, s), header = FALSE))))
+census94 <- data.frame(do.call(rbind, lapply(list.files(RAW_DATA_PATH), function(s) read.csv(paste0(RAW_DATA_PATH, s), header = FALSE, na.strings = "?", strip.white = TRUE))))
 
 colnames(census94) <- c("age", "workclass", "fnlwgt", "education", "education_num", "marital_status", "occupation", "relationship", "race", "sex", "capital_gain", "capital_loss", "hours_per_week", "native_country", "income")
 
